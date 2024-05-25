bdd2089b0c943cf2241222da075313cb115461e3
sunny kumar <sunnykumar2020has@gmail.com>
2024-05-25 16:04:25 +0530

    changes saved

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=" bg-white py-4 stick top-0 z-10">
+      <div className=" container "></div>
+
+    </div>
+  )
+}
+
+export default NavBar
